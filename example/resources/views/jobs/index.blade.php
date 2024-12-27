ecb263b50c1d145a56fc143334fa1299105b1661
kenntrix <kenntrixwambuzz@gmail.com>
2024-12-27 22:57:47 +0300

    first commit

---
+++ example/resources/views/jobs/index.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <x-slot:heading>
+        Job Listing
+    </x-slot:heading>
+
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}"class="block px-4 py-6 border border-gray-300 rounded-lg">
+                <div class="font-bold text-blue-500 txt-sm">{{ $job->employer->name }}</div>
+                <div>
+                    <strong>{{$job['title']}}:</strong> Pays {{ $job['salary']}} per year.
+                </div>
+            </a> 
+        @endforeach
+
+        <div>
+            {{ $jobs->links()}}
+        </div>
+    </div>
+</x-layout>
---
+++ example/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\JobController;
+use App\Models\Job;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+Route::resource('jobs', JobController::class);
+
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}/', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
